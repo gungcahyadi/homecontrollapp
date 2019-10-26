31c3471c68af1720610f4d444cd4135d6a989917
gungcahyadi <gungcahyadipp@gmail.com>
2019-10-26 13:50:42 +0800

    add profile

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import RemoteControl from './components/RemoteControl';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello world</h2>
-      <RemoteControl />
+      <UserProfile />
     </div>
   );
 }
---
+++ src/components/UserProfile.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const UserProfile = () => {
+    return (
+        <div className="UserProfile">
+          <h2>Nama : Gung Cahyadi</h2>
+          <h2>Fb Link : <a target="_blank" href="https://facebook.com/gungcah">https://facebook.com/gungcah</a></h2>
+          <h2>GitHub Link : <a target="_blank" href="https://github.com/gungcahyadi">https://github.com/gungcahyadi</a></h2>
+        </div>
+      );
+}
+
+export default UserProfile;
